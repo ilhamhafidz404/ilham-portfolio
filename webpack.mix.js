fe5fa87e75cdb2b273a9b722761f2576f9b45c81
ilhamhafidz404 <xxspanzie@gmail.com>
2021-12-08 14:57:14 +0700

    Lets start this Project

---
+++ webpack.mix.js
@@ -0,0 +1,7 @@
+// webpack.mix.js
+
+let mix = require("laravel-mix");
+
+mix.sass("resources/sass/app.scss", "css").setPublicPath("public");
+mix.css("resources/css/tailwind.css", "css").setPublicPath("public");
+mix.js("resources/js/alphine.js", "js").setPublicPath("public");

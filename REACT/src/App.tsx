82c8fba7f3685a6e447aa4332493648b18450624
ilhamhafidz404 <xxspanzie@gmail.com>
2024-09-08 11:56:53 +0700

    Remake Home page

--- REACT/src/App.tsx
+++ REACT/src/App.tsx
@@ -25,139 +25,186 @@ function App() {
   const [projectCategory, setProjectCategory] = useState(1);
 
   return (
-    <main className="text-gray-800">
-      <Navbar />
+    <>
+      <section className="grid grid-cols-5 px-20 gap-16 mt-20">
+        <aside>
+          <ul>
+            <li className="flex items-center gap-5 bg-gray-200 px-5 py-3 rounded-md hover:bg-gray-200 mb-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
+              </svg>
+              Home
+            </li>
+            <li className="flex items-center gap-5 px-5 py-3 rounded-md hover:bg-gray-200 mb-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
+              </svg>
+              About
+            </li>
+          </ul>
+        </aside>
+        <main className="text-gray-800 col-span-4">
+          {/* <Navbar /> */}
 
-      <Header />
+          <Header />
 
-      <section className="bg-gray-100 xl:pb-5 pb-20" id="about">
-        <div className="container xl:min-h-screen">
-          <TitleSection title="ABOUT" />
-          <div className="grid lg:grid-cols-3">
-            {/* <QuoteCard /> */}
-            <div>
-              <img
-                src="/images/ilham.JPG"
-                alt="ilham-hafidz"
-                className="rounded border p-2 lg:w-full sm:w-[400px] w-[300px] lg:h-full sm:h-[400px] h-[300px] object-cover object-bottom mx-auto"
-              />
-            </div>
-            <div className="lg:col-span-2 flex items-center">
-              <section className="lg:-ml-[50px] lg:mt-0 -mt-[70px] bg-white sm:p-10 p-5 rounded shadow-md">
-                <h5 className="myfont-montserrat mytext-primary">
-                  Hello Guys, I am Ilham Hafidz
-                </h5>
-                <h4 className="sm:text-3xl text-2xl font-bold tracking-wide myfont-montserrat dark:text-gray-100">
-                  FULLSTACK WEB DEVELOPER
-                </h4>
-                <p className="mt-5 text-gray-800 myfont-roboto-mono dark:text-gray-200 sm:text-base text-sm">
-                  Saya telah mengerjakan beberapa proyek, baik secara individu
-                  maupun dalam tim. Ketika mengerjakan proyek individu, saya
-                  terbiasa menangani desain frontend dan backend sebuah website.
-                  Biasanya, saya menggunakan Laravel dan VueJS untuk
-                  proyek-proyek tersebut. Sementara itu, dalam proyek tim, saya
-                  lebih fokus dan mahir di bidang frontend.
-                </p>
-
-                <div className="lg:flex sm:block hidden gap-5 min-h-[180px]">
-                  <div>
-                    <h3 className="mt-10 font-semibold">This is my Skills</h3>
-                    <SkillDock data={dataSkill} />
-                  </div>
-                  <div>
-                    <h3 className="mt-10 font-semibold">This is my Tools</h3>
-                    <SkillDock data={dataTool} />
-                  </div>
-                </div>
-
-                <div className="mt-10 inline-flex flex-col sm:flex-row items-center w-full">
-                  <Button
-                    text="Download CV"
-                    target=""
-                    icon={<DownloadIcon />}
-                    addedClass="mr-4"
-                  />
-                  <br />
-                  <Button
-                    text="Hire Me"
-                    target="mailto:ilhammhafidzz@gmail.com"
-                    type="outline"
+          <section className="bg-gray-100 xl:pb-5 pb-20" id="about">
+            <div className="container xl:min-h-screen">
+              <TitleSection title="ABOUT" />
+              <div className="grid lg:grid-cols-3">
+                {/* <QuoteCard /> */}
+                <div>
+                  <img
+                    src="/images/ilham.JPG"
+                    alt="ilham-hafidz"
+                    className="rounded border p-2 lg:w-full sm:w-[400px] w-[300px] lg:h-full sm:h-[400px] h-[300px] object-cover object-bottom mx-auto"
                   />
                 </div>
-              </section>
+                <div className="lg:col-span-2 flex items-center">
+                  <section className="lg:-ml-[50px] lg:mt-0 -mt-[70px] bg-white sm:p-10 p-5 rounded shadow-md">
+                    <h5 className="myfont-montserrat mytext-primary">
+                      Hello Guys, I am Ilham Hafidz
+                    </h5>
+                    <h4 className="sm:text-3xl text-2xl font-bold tracking-wide myfont-montserrat dark:text-gray-100">
+                      FULLSTACK WEB DEVELOPER
+                    </h4>
+                    <p className="mt-5 text-gray-800 myfont-roboto-mono dark:text-gray-200 sm:text-base text-sm">
+                      Saya telah mengerjakan beberapa proyek, baik secara
+                      individu maupun dalam tim. Ketika mengerjakan proyek
+                      individu, saya terbiasa menangani desain frontend dan
+                      backend sebuah website. Biasanya, saya menggunakan Laravel
+                      dan VueJS untuk proyek-proyek tersebut. Sementara itu,
+                      dalam proyek tim, saya lebih fokus dan mahir di bidang
+                      frontend.
+                    </p>
+
+                    <div className="lg:flex sm:block hidden gap-5 min-h-[180px]">
+                      <div>
+                        <h3 className="mt-10 font-semibold">
+                          This is my Skills
+                        </h3>
+                        <SkillDock data={dataSkill} />
+                      </div>
+                      <div>
+                        <h3 className="mt-10 font-semibold">
+                          This is my Tools
+                        </h3>
+                        <SkillDock data={dataTool} />
+                      </div>
+                    </div>
+
+                    <div className="mt-10 inline-flex flex-col sm:flex-row items-center w-full">
+                      <Button
+                        text="Download CV"
+                        target=""
+                        icon={<DownloadIcon />}
+                        addedClass="mr-4"
+                      />
+                      <br />
+                      <Button
+                        text="Hire Me"
+                        target="mailto:ilhammhafidzz@gmail.com"
+                        type="outline"
+                      />
+                    </div>
+                  </section>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          </section>
+
+          {/*  */}
+
+          <section className="container" id="project">
+            <TitleSection title="PROJECT" />
+
+            <div className="mt-1 mb-20 text-center">
+              <div className="gap-5 inline-flex sm:flex-row flex-col justify-center border rounded border-gray-100 bg-gray-100">
+                <button
+                  className={`flex gap-2 relative hover:mybg-primary-hovered px-5 py-2 rounded ${
+                    projectCategory === 1
+                      ? "mytext-primary hover:bg-transparent font-semibold after:content-[''] after:absolute after:mybg-primary after:opacity-20 after:rounded after:scale-100 after:h-[calc(100%+10px)] after:w-[calc(100%+5px)] after:block after:left-[-2.5px] after:top-[-5px]  scale-110"
+                      : ""
+                  }`}
+                  onClick={() => setProjectCategory(1)}
+                >
+                  <TrophyIcon />
+                  Perlombaan
+                </button>
+
+                <button
+                  className={`flex gap-2 relative hover:mybg-primary-hovered px-5 py-2 rounded ${
+                    projectCategory === 2
+                      ? "mytext-primary hover:bg-transparent font-semibold after:content-[''] after:absolute after:mybg-primary after:opacity-20 after:rounded after:scale-100 after:h-[calc(100%+10px)] after:w-[calc(100%+5px)] after:block after:left-[-2.5px] after:top-[-5px]  scale-110"
+                      : ""
+                  }`}
+                  onClick={() => setProjectCategory(2)}
+                >
+                  <Briefcase />
+                  Pekerjaan
+                </button>
+                <button
+                  className={`flex gap-2  relative hover:mybg-primary-hovered px-5 py-2 rounded ${
+                    projectCategory === 3
+                      ? "mytext-primary hover:bg-transparent font-semibold after:content-[''] after:absolute after:mybg-primary after:opacity-20 after:rounded after:scale-100 after:h-[calc(100%+10px)] after:w-[calc(100%+5px)] after:block after:left-[-2.5px] after:top-[-5px]  scale-110"
+                      : ""
+                  }`}
+                  onClick={() => setProjectCategory(3)}
+                >
+                  <UserIcon />
+                  Individu
+                </button>
+              </div>
+            </div>
+
+            {projects
+              .filter((project) => project.category === projectCategory)
+              .map((project, index) => (
+                <ProjectCard
+                  key={index} // Jangan lupa tambahkan key untuk elemen yang diulang
+                  order={index + 1}
+                  title={project.title}
+                  subtitle={project.subtitle}
+                  description={project.description}
+                  photoSource={project.photo}
+                  preview={project.preview}
+                  github={project.github}
+                />
+              ))}
+          </section>
+
+          {/*  */}
+
+          <footer className="container py-5 mt-28 border-t myborder-primary">
+            <p className="myfont-roboto-mono text-gray-800 text-sm">
+              &copy; Copyright Ilham Hafidz
+            </p>
+          </footer>
+        </main>
       </section>
-
-      {/*  */}
-
-      <section className="container" id="project">
-        <TitleSection title="PROJECT" />
-
-        <div className="mt-1 mb-20 text-center">
-          <div className="gap-5 inline-flex sm:flex-row flex-col justify-center border rounded border-gray-100 bg-gray-100">
-            <button
-              className={`flex gap-2 relative hover:mybg-primary-hovered px-5 py-2 rounded ${
-                projectCategory === 1
-                  ? "mytext-primary hover:bg-transparent font-semibold after:content-[''] after:absolute after:mybg-primary after:opacity-20 after:rounded after:scale-100 after:h-[calc(100%+10px)] after:w-[calc(100%+5px)] after:block after:left-[-2.5px] after:top-[-5px]  scale-110"
-                  : ""
-              }`}
-              onClick={() => setProjectCategory(1)}
-            >
-              <TrophyIcon />
-              Perlombaan
-            </button>
-
-            <button
-              className={`flex gap-2 relative hover:mybg-primary-hovered px-5 py-2 rounded ${
-                projectCategory === 2
-                  ? "mytext-primary hover:bg-transparent font-semibold after:content-[''] after:absolute after:mybg-primary after:opacity-20 after:rounded after:scale-100 after:h-[calc(100%+10px)] after:w-[calc(100%+5px)] after:block after:left-[-2.5px] after:top-[-5px]  scale-110"
-                  : ""
-              }`}
-              onClick={() => setProjectCategory(2)}
-            >
-              <Briefcase />
-              Pekerjaan
-            </button>
-            <button
-              className={`flex gap-2  relative hover:mybg-primary-hovered px-5 py-2 rounded ${
-                projectCategory === 3
-                  ? "mytext-primary hover:bg-transparent font-semibold after:content-[''] after:absolute after:mybg-primary after:opacity-20 after:rounded after:scale-100 after:h-[calc(100%+10px)] after:w-[calc(100%+5px)] after:block after:left-[-2.5px] after:top-[-5px]  scale-110"
-                  : ""
-              }`}
-              onClick={() => setProjectCategory(3)}
-            >
-              <UserIcon />
-              Individu
-            </button>
-          </div>
-        </div>
-
-        {projects
-          .filter((project) => project.category === projectCategory)
-          .map((project, index) => (
-            <ProjectCard
-              key={index} // Jangan lupa tambahkan key untuk elemen yang diulang
-              order={index + 1}
-              title={project.title}
-              subtitle={project.subtitle}
-              description={project.description}
-              photoSource={project.photo}
-              preview={project.preview}
-              github={project.github}
-            />
-          ))}
-      </section>
-
-      {/*  */}
-
-      <footer className="container py-5 mt-28 border-t myborder-primary">
-        <p className="myfont-roboto-mono text-gray-800 text-sm">
-          &copy; Copyright Ilham Hafidz
-        </p>
-      </footer>
-    </main>
+    </>
   );
 }
 
--- REACT/src/components/Header/Header.tsx
+++ REACT/src/components/Header/Header.tsx
@@ -1,49 +1,166 @@
 import SparklesText from "../../../@/components/magicui/sparkles-text";
 import Button from "../Button";
+import { SkillDock } from "../Dock";
 import DocumentIcon from "../Icons/Document";
 import PhotoCard from "./PhotoCard";
 
+import dataSkill from "../../../public/data/skills.json";
+
 export default function Header() {
   return (
-    <section id="home" className="container mx-auto h-screen flex items-center">
-      <header className="myfont-montserrat px-0">
-        <div className="flex lg:flex-row flex-col-reverse items-center">
-          <div className="flex items-center justify-center lg:w-1/2 w-full sm:mt-0 mt-10">
-            <div className="pl-0 lg:text-left text-center">
-              <h3 className="mytext-primary tracking-tighter text-xl">
-                Hello I am
-              </h3>
-              <SparklesText
-                text="Ilham Hafidz"
-                className="uppercase lg:text-6xl text-4xl font-bold tracking-tight"
-                sparklesCount={3}
-                colors={{ first: "#d65d26", second: "#d65d26" }}
-              />
-              <p className="myfont-roboto-mono mt-7 mb-10 text-xs sm:text-sm lg:text-base lg:p-0 px-6 ">
-                Saya adalah web programmer dengan pengalaman empat tahun, selama
-                waktu tersebut saya telah terlibat dalam berbagai proyek, mulai
-                dari membangun situs web responsif hingga mengembangkan aplikasi
-                web kompleks.
-              </p>
-              <div className="mt-7 sm:flex gap-3 lg:justify-start justify-center">
-                <Button
-                  text="Lihat Tentang Saya"
-                  target="https://github.com/ilhamhafidz404?tab=repositories"
-                />
-                <br className="sm:hidden block" />
-                <Button
-                  text="Download CV"
-                  target="mailto:ilhammhafidzz@gmail.com"
-                  type="outline"
-                  icon={<DocumentIcon />}
-                  addedClass="gap-1 sm:mt-0 mt-3"
-                />
-              </div>
-            </div>
-          </div>
-          <PhotoCard />
+    <section className="h-screen">
+      <div className="relative">
+        <h2 className="text-4xl font-semibold text-indigo-500">ILHAM HAFIDZ</h2>
+        {/* <h1 className="text-6xl font-bold tracking-wide leading-tight after:content-[''] after:bg-gradient-to-r after:from-indigo-500 after:to-indigo-100 after:w-[680px] after:h-[20px] after:absolute after:left-12 after:bottom-3 after:-z-10">
+          a Fullstack Developer
+        </h1> */}
+        <h1 className="text-6xl font-bold tracking-wide leading-tight">
+          a Fullstack Developer
+        </h1>
+      </div>
+      <div className="mt-5">
+        <p className="leading-loose">
+          Aspiring Web Developer Student with a passion for crafting
+          pixel-perfect web experiences. I'm deeply invested in the world of web
+          development, particularly using JavaScript. I thrive in collaborative
+          environments, working alongside teams to bring efficient, scalable,
+          and visually stunning web applications to life.
+        </p>
+      </div>
+      <hr className="mt-10" />
+      <div className="mt-10">
+        <h4 className="text-xl font-medium mb-4">Tools That I Have Used</h4>
+        {/* <SkillDock data={dataSkill} /> */}
+        <div className="flex gap-3">
+          <span className="inline-flex gap-2 items-center border rounded-xl px-4 py-2 text-xs font-medium">
+            <img
+              src="/images/skills/bootstrap.png"
+              alt="Bootstrap"
+              className="w-[25px]"
+            />
+            Bootstrap
+          </span>
+          <span className="inline-flex gap-2 items-center border rounded-xl px-4 py-2 text-xs font-medium">
+            <img
+              src="/images/skills/tailwind.png"
+              alt="Tailwind"
+              className="w-[25px]"
+            />
+            Tailwind
+          </span>
+          <span className="inline-flex gap-2 items-center border rounded-xl px-4 py-2 text-xs font-medium">
+            <img
+              src="/images/skills/vue.png"
+              alt="Vue JS"
+              className="w-[25px]"
+            />
+            Vue JS
+          </span>
+          <span className="inline-flex gap-2 items-center border rounded-xl px-4 py-2 text-xs font-medium">
+            <img
+              src="/images/skills/react.png"
+              alt="React JS"
+              className="w-[25px]"
+            />
+            React JS
+          </span>
+          <span className="inline-flex gap-2 items-center border rounded-xl px-4 py-2 text-xs font-medium">
+            <img
+              src="/images/skills/laravel.png"
+              alt="Laravel"
+              className="w-[25px]"
+            />
+            Next JS
+          </span>
+          <span className="inline-flex gap-2 items-center border rounded-xl px-4 py-2 text-xs font-medium">
+            <img
+              src="/images/skills/golang.png"
+              alt="Golang"
+              className="w-[25px]"
+            />
+            Golang
+          </span>
+          <span className="inline-flex gap-2 items-center border rounded-xl px-4 py-2 text-xs font-medium">
+            <img
+              src="/images/skills/laravel.png"
+              alt="Laravel"
+              className="w-[25px]"
+            />
+            Laravel
+          </span>
+          <span className="inline-flex gap-2 items-center border rounded-xl px-4 py-2 text-xs font-medium">
+            <img
+              src="/images/skills/laravel.png"
+              alt="Laravel"
+              className="w-[25px]"
+            />
+            Express JS
+          </span>
         </div>
-      </header>
+      </div>
+      <hr className="mt-10" />
+      <h4 className="text-xl font-medium mt-10">Stats</h4>
+      <div className="grid grid-cols-3 gap-10 mt-5">
+        <div>
+          <h5 className="text-5xl font-bold">4+</h5>
+          <h6 className="font-semibold mb-5">Year Experience</h6>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        </div>
+        <div>
+          <h5 className="text-5xl font-bold">10</h5>
+          <h6 className="font-semibold mb-5">Happy Client</h6>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae,
+          </p>
+        </div>
+        <div>
+          <h5 className="text-5xl font-bold">10</h5>
+          <h6 className="font-semibold mb-5">Happy Client</h6>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae,
+          </p>
+        </div>
+      </div>
     </section>
+    // <section id="home" className="container mx-auto h-screen flex items-center">
+    //   <header className="myfont-montserrat px-0">
+    //     <div className="flex lg:flex-row flex-col-reverse items-center">
+    //       <div className="flex items-center justify-center lg:w-1/2 w-full sm:mt-0 mt-10">
+    //         <div className="pl-0 lg:text-left text-center">
+    //           <h3 className="mytext-primary tracking-tighter text-xl">
+    //             Hello I am
+    //           </h3>
+    //           <SparklesText
+    //             text="Ilham Hafidz"
+    //             className="uppercase lg:text-6xl text-4xl font-bold tracking-tight"
+    //             sparklesCount={3}
+    //             colors={{ first: "#d65d26", second: "#d65d26" }}
+    //           />
+    //           <p className="myfont-roboto-mono mt-7 mb-10 text-xs sm:text-sm lg:text-base lg:p-0 px-6 ">
+    //             Saya adalah web programmer dengan pengalaman empat tahun, selama
+    //             waktu tersebut saya telah terlibat dalam berbagai proyek, mulai
+    //             dari membangun situs web responsif hingga mengembangkan aplikasi
+    //             web kompleks.
+    //           </p>
+    //           <div className="mt-7 sm:flex gap-3 lg:justify-start justify-center">
+    //             <Button
+    //               text="Lihat Tentang Saya"
+    //               target="https://github.com/ilhamhafidz404?tab=repositories"
+    //             />
+    //             <br className="sm:hidden block" />
+    //             <Button
+    //               text="Download CV"
+    //               target="mailto:ilhammhafidzz@gmail.com"
+    //               type="outline"
+    //               icon={<DocumentIcon />}
+    //               addedClass="gap-1 sm:mt-0 mt-3"
+    //             />
+    //           </div>
+    //         </div>
+    //       </div>
+    //       <PhotoCard />
+    //     </div>
+    //   </header>
+    // </section>
   );
 }
